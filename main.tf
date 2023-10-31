aba73c8fec841810971a0a3441f762598c2347e2
Shivam singh <89725420+shivam030699@users.noreply.github.com>
2023-10-31 12:31:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+provider "azurerm" {
+    version         =   "~> 2.0"
+    client_id       =   var.client_id
+    client_secret   =   var.client_secret
+    subscription_id =   var.subscription_id
+    tenant_id       =   var.tenant_id
+    
+    features {}
+}
+
+provider "azuread" {
+    version         =   ">= 0.11"
+    client_id       =   var.client_id
+    client_secret   =   var.client_secret
+    tenant_id       =   var.tenant_id
+    alias           =   "ad"
+}
+
 resource "azurerm_app_service" "webapp" {
   name                = "Terraform-web-app"
   location            = "Central US"
