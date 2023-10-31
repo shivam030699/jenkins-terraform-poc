97d02437deb6742cbfe1ce4f17231ac1632b3521
Shivam singh <89725420+shivam030699@users.noreply.github.com>
2023-10-31 12:16:47 +0530

    Update main.tf
    Added code for web apps.

---
+++ main.tf
@@ -0,0 +1,6 @@
+resource "azurerm_app_service" "webapp" {
+  name                = "Terraform-web-app"
+  location            = "Central US"
+  resource_group_name = "EdgyneerEnhancementV2"
+  app_service_plan_id = "/subscriptions/fd57b649-61f3-4029-a3cd-a6e9aa0b4d80/resourceGroups/EdgyneerEnhancementV2/providers/Microsoft.Web/serverfarms/ASP-rgvanderlendedev001-8a0e"
+}
