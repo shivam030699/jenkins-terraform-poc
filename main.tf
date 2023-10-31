b76449c0939f4cf09c83aadf5d6f894454a89163
Shivam singh <89725420+shivam030699@users.noreply.github.com>
2023-10-31 12:35:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 provider "azurerm" {
-    version         =   "~> 2.0"
     client_id       =   var.client_id
     client_secret   =   var.client_secret
     subscription_id =   var.subscription_id
@@ -9,7 +8,6 @@ provider "azurerm" {
 }
 
 provider "azuread" {
-    version         =   ">= 0.11"
     client_id       =   var.client_id
     client_secret   =   var.client_secret
     tenant_id       =   var.tenant_id
